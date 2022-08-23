c5bcc94569e4d4e11172de5d67bc5bedd966b88c
Gabriel Pereira <gabriel.psouza1997@outlook.com>
2022-08-22 21:51:02 -0300

    Service layer, Component registration

--- src/main/java/com/udemy/coursespringboot/resources/UserResource.java
+++ src/main/java/com/udemy/coursespringboot/resources/UserResource.java
@@ -1,22 +1,34 @@
 package com.udemy.coursespringboot.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.udemy.coursespringboot.entities.User;
+import com.udemy.coursespringboot.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired
+	UserService us;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User p = new User("Gabriel", "gabriel@gmail", 2L, "99999", "12345");
-		return ResponseEntity.ok(p);
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list;
+		list = us.findAll();
+		return ResponseEntity.ok(list);
 		
-		
+	}
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		return ResponseEntity.ok(us.findById(id));
 	}
 
 	
